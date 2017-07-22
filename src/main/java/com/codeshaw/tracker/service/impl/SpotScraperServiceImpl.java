2f93ebc124390d2c30c4afab16f7b2b02d455f7d
U-MAGI\Brad <tom@codeshaw.com>
2017-07-22 15:19:39 +0100

    Added single-response json so I can mess with it later

--- src/main/java/com/codeshaw/tracker/service/impl/SpotScraperServiceImpl.java
+++ src/main/java/com/codeshaw/tracker/service/impl/SpotScraperServiceImpl.java
@@ -1,15 +1,18 @@
 package com.codeshaw.tracker.service.impl;
 
+import com.codeshaw.tracker.dto.spot.Response;
 import com.codeshaw.tracker.dto.spot.SpotResponse;
 import com.codeshaw.tracker.mapping.impl.SpotResponseToCheckInMapper;
 import com.codeshaw.tracker.repository.CheckInRepository;
 import com.codeshaw.tracker.repository.SharedPageRepository;
 import com.codeshaw.tracker.service.SpotScraperService;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.joda.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -77,20 +80,15 @@ public class SpotScraperServiceImpl implements SpotScraperService {
      * @return The SPOT messages as DTO
      */
     private SpotResponse fetchFeedWithId(String sharedPageFeedId) {
-        return new RestTemplate().getForObject(String.format(API_URL, sharedPageFeedId), SpotResponse.class);
+        // Spot's API is crap. If there's only one element it returns one element, not an array
+        // And I don't have the time to work out how to mitigate it. Busy busy busy.
+        try {
+            return new RestTemplate().getForObject(String.format(API_URL, sharedPageFeedId), SpotResponse.class);
+        } catch (HttpMessageNotReadableException e) {
+            SpotResponse spotResponse = new SpotResponse();
+            spotResponse.setResponse(new Response());
+            return spotResponse;
+        }
     }
 
-//    /**
-//     * Crap test method for the file system.
-//     * @param sharedPageFeedId
-//     * @return
-//     */
-//    @Autowired private ObjectMapper objectMapper;
-//    private SpotResponse fetchFeedWithId(String sharedPageFeedId) {
-//        try {
-//            return objectMapper.readValue(new URL("classpath:response.json"), SpotResponse.class);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
