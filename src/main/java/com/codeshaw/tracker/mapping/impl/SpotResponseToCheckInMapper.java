b40fb38709adbb5af44f9185b82da4c16c5cbeeb
U-MAGI\Brad <tom@codeshaw.com>
2017-07-22 13:59:13 +0100

    Fixed a problem with an NPE when there are no check-in events

--- src/main/java/com/codeshaw/tracker/mapping/impl/SpotResponseToCheckInMapper.java
+++ src/main/java/com/codeshaw/tracker/mapping/impl/SpotResponseToCheckInMapper.java
@@ -2,6 +2,7 @@ package com.codeshaw.tracker.mapping.impl;
 
 import com.codeshaw.tracker.domain.CheckIn;
 import com.codeshaw.tracker.domain.SharedPage;
+import com.codeshaw.tracker.dto.spot.Response;
 import com.codeshaw.tracker.dto.spot.SpotResponse;
 import com.codeshaw.tracker.mapping.OneToManyMapper;
 import com.codeshaw.tracker.repository.SharedPageRepository;
@@ -10,6 +11,7 @@ import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,17 +23,22 @@ public class SpotResponseToCheckInMapper implements OneToManyMapper<SpotResponse
 
   @Override
   public List<CheckIn> getMappedList(SpotResponse dto) {
-    String sharedFeedId = dto.getResponse().getFeedMessageResponse().getFeed().getId();
+    Response response = dto.getResponse();
+    if (response.getFeedMessageResponse() == null) {
+      return new ArrayList<>();
+    }
+
+    String sharedFeedId = response.getFeedMessageResponse().getFeed().getId();
     SharedPage sharedPage = sharedPageRepository.findOne(sharedFeedId);
 
     return dto.getResponse().getFeedMessageResponse().getMessages().getMessage().stream()
-            .map(currentMessage -> {
-              CheckIn checkIn = new CheckIn();
-              checkIn.setSharedPage(sharedPage);
-              checkIn.setCheckInTime(new LocalDateTime(currentMessage.getUnixTime() * 1000, DateTimeZone.UTC));
-              checkIn.setLatitude(currentMessage.getLatitude());
-              checkIn.setLongitude(currentMessage.getLongitude());
-              return checkIn;
-            }).collect(Collectors.toList());
+        .map(currentMessage -> {
+          CheckIn checkIn = new CheckIn();
+          checkIn.setSharedPage(sharedPage);
+          checkIn.setCheckInTime(new LocalDateTime(currentMessage.getUnixTime() * 1000, DateTimeZone.UTC));
+          checkIn.setLatitude(currentMessage.getLatitude());
+          checkIn.setLongitude(currentMessage.getLongitude());
+          return checkIn;
+        }).collect(Collectors.toList());
   }
 }
