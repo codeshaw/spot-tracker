52e75c4a4655d2a915d9d9de687f95cc7e9df71c
brad <brad@example.com>
2017-09-13 18:47:29 +0300

    Added unlimited track enum

--- src/main/java/com/codeshaw/tracker/dto/spot/MessageType.java
+++ src/main/java/com/codeshaw/tracker/dto/spot/MessageType.java
@@ -1,11 +1,13 @@
 package com.codeshaw.tracker.dto.spot;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum MessageType {
   OK,
   TRACK,
+  @JsonProperty("UNLIMITED-TRACK") UNLIMITED_TRACK,
   CUSTOM,
   HELP,
   HELP_CANCEL;
